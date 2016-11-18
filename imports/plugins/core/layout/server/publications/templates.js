e88869a9d136bffa556503c2f8aa2c71b569f3b4
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2016-11-18 09:43:07 -0800

    add templates count for Griddle

--- imports/plugins/core/layout/server/publications/templates.js
+++ imports/plugins/core/layout/server/publications/templates.js
@@ -1,13 +1,34 @@
-import { Reaction } from "/server/api";
 import { Meteor } from "meteor/meteor";
+import { Counts } from "meteor/tmeasday:publish-counts";
 import { Templates } from "/lib/collections";
+import { Reaction } from "/server/api";
+
+//
+// Security
+// import "/server/security/collections";
+// Security definitions
+//
+Security.permit(["insert", "update", "remove"]).collections([
+  Templates
+]).ifHasRole({
+  role: "admin",
+  group: Reaction.getShopId()
+});
 
 Meteor.publish("Templates", () => {
+  // Check shopId
   const shopId = Reaction.getShopId();
-
   if (!shopId) {
     return this.ready();
   }
+
+  // appends a count to the collection
+  // we're doing this for use with griddleTable
+  Counts.publish(this, "templates-count", Templates.find(
+    shopId
+    // select,
+    // options
+  ));
 
   return Templates.find({
     shopId
